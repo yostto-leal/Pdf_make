0ccb7d66095811effa66eb57b5f6c869b05f098e
yostto-leal <lealariosto@gmail.com>
2025-09-09 19:31:19 -0300

    feat:coorecao vite,config,js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  base: '/Pdf_make/', // 👈 isso precisa bater com o nome do repositório
 })
